1dfc265332d4aeffdf0cf839a3fc98f2225d9b78
Anshul Sirur <anshul.sirur@digital.cabinet-office.gov.uk>
2016-03-03 17:01:21 +0000

    refactoring stuff

--- rabble.js
+++ rabble.js
@@ -1,59 +1,119 @@
 window.onload = function () {
-  var
-    in_minutes = document.getElementById('in_minutes'),
-    in_break = document.getElementById('in_break'),
-    in_new_member = document.getElementById('in_new_member'),
-    btn_start = document.getElementById('btn_start'),
-    btn_reset = document.getElementById('btn_reset'),
-    ul_gang = document.getElementById('gang_list'),
-    div_project = document.getElementById('project'),
-    div_timer = document.getElementById('timer'),
-    div_cycle = document.getElementById('cycle');
-    div_scream = document.getElementById('screamout');
+  function isRunning() { 
+    return ('RABBLE_RUN' in window)? window.RABBLE_RUN : false;
+  }
 
-  var set_minutes = parseFloat(in_minutes.value);
-  var set_break = parseInt(in_break.value);
-  var members = [];
-  var running = false;
-  var paused = false;
-  var notify = false;
+  function setRunning(bool) { 
+    window.RABBLE_RUN = bool;
+  }
 
-  var driver, navigator;
+  function updateTimer(seconds) {
+    var div_timer = document.getElementById('timer');
+    var mins = Math.floor(seconds / 60);
+    var secs = seconds % 60;
+    div_timer.textContent = mins + ':' + (secs < 10)? '0' + secs : secs;
+  }
 
-  var seconds = 0;
-  var cycle = 0;
+  function updateCycle(cycle) {
+    document.getElementById('cycle').textContent = 'Cycle #' + cycle;
+  }
 
-  var memberId = function(el) {
+  function updateStartButton(text, action) {
+    var btn_start = document.getElementById('btn_start');
+    btn_start.textContent = text;
+    btn_start.removeEventListener('click');
+    btn_start.addEventListener('click', action);
+  }
+
+  function memberId(el) {
     return parseInt(el.getAttribute('id').replace('member-',''));
-  };
+  }
 
-  var memberEl = function(id) {
+  function memberEl(id) {
     return document.getElementById('member-'+id);
   }
 
-  var resetSeconds = function () {
-    set_minutes = parseFloat(in_minutes.value);
-    seconds = Math.floor(set_minutes * 60);
-    updateTimer();
+  function getSetSeconds() {
+    var set_minutes = parseFloat(document.getElementById('in_minutes').value) || 10.0;
+    return Math.floor(set_minutes * 60);
   };
 
-  var updateTimer = function () {
-    var mins = Math.floor(seconds / 60);
-    var secs = seconds % 60;
-        secs = (secs < 10)? '0' + secs : secs;
-    div_timer.textContent = mins + ':' + secs;
+  function getBreakCycles() {
+    return parseInt(document.getElementById('in_break').value) || 6;
   };
+
+  function init() {
+    var btn_start = document.getElementById('btn_start');
+    updateStartButton('Start', start);
+  }
+
+  function unpause() {
+     
+  }
+
+  function pause() {
+    setRunning(false);
+  }
+
+  function stop() {
+    setRunning(false);
+  }
+
+  function start() {
+    setRunning(true);
+    update(0, 0);
+  }
+
+  function update(current_cycle, elapsed_seconds) {
+    var cycle_seconds = getSetSeconds();
+    var break_cycles = getBreakCycles();
+
+    if (elapsed_seconds > cycle_seconds) {
+      current_cycle += 1;
+      elapsed_seconds = 0;
+
+      if (current_cycle % break_cycles == 0) {
+        breakTime();
+      } else {
+        nextCycle();
+      }
+    }
+
+    if (isRunning()) {
+      elapsed_seconds += 1;
+    }
+
+    window.setTimeout(update, 1000, current_cycle, elapsed_seconds);
+  }
+
+  var
+    in_break = document.getElementById('in_break'),
+    in_new_member = document.getElementById('in_new_member'),
+    btn_reset = document.getElementById('btn_reset'),
+    ul_gang = document.getElementById('gang_list'),
+    div_scream = document.getElementById('screamout'),
+
+    set_minutes = parseFloat(in_minutes.value),
+    set_break = parseInt(in_break.value),
+    members = [],
+    running = false,
+    paused = false,
+    notify = false,
+    seconds = 0,
+    cycle = 0
+    driver, 
+    navigator
+    ;
 
   var updatePair = function () {
     if (members.length >= 2) {
       var nm = members.length;
-      var navigator_id = cycle % nm;
-      var driver_id = (cycle+1) % nm;
-      navigator = members[navigator_id];
+      var driver_id = cycle % nm;
       driver = members[driver_id];
       members.forEach(function (v,i) { memberEl(i).classList.remove('driver', 'navigator') });
       memberEl(driver_id).classList.add('driver');
-      memberEl(navigator_id).classList.add('navigator');
+      // navigator = members[navigator_id];
+      // memberEl(navigator_id).classList.add('navigator');
     }
   };
 
@@ -79,194 +139,188 @@ window.onload = function () {
 
     if (notify) {
       var notification = new Notification(scream_title, {'body':scream_body});
+      notification.addEventListener('click', function () {
+        hitStart();
+      })
     }
-  };
-
-  var updateCycle = function () {
-    if (seconds % (set_minutes * 60) === 0) {
-      resetSeconds();
-      pause('Continue');
-      updatePair();
-      scream();
-      cycle++;
-    }
-    div_cycle.textContent = 'Cycle #' + cycle;
-  };
+  }
 
   var updateList = function () {
-    while (ul_gang.firstChild) ul_gang.removeChild(ul_gang.firstChild);
+    while (ul_gang.firstChild) ul_gang.removeChild(ul_gang.firstChild)
 
     members.forEach(function (name, id) {
-      var li = document.createElement('li');
-      li.textContent = name;
-      li.classList.add('member_item');
-      li.setAttribute('draggable', 'true');
-      li.setAttribute('id', 'member-'+id);
+      var li = document.createElement('li')
+      li.textContent = name
+      li.classList.add('member_item')
+      li.setAttribute('draggable', 'true')
+      li.setAttribute('id', 'member-'+id)
 
       // Double click - delete
       li.addEventListener('dblclick', function () {
-        var el_id = memberId(li);
-        members.splice(el_id, 1);
-        updateList();
-      });
+        var el_id = memberId(li)
+        members.splice(el_id, 1)
+        updateList()
+      })
 
-      ul_gang.appendChild(li);
-    });
-  };
+      ul_gang.appendChild(li)
+    })
+  }
 
   var update = function () {
     if (running && !paused) {
-      updateCycle();
-      updateTimer();
-      seconds--;
+      if (seconds % (set_minutes * 60) === 0) {
+        resetSeconds()
+        pause('Continue')
+        updatePair()
+        scream()
+        cycle++
+      }
+      updateTimer(seconds)
+      seconds--
     }
-    window.setTimeout(update, 1000);
-  };
+    window.setTimeout(update, 1000)
+  }
 
   var unpause = function () {
-    paused = false;
-    btn_start.textContent = 'Pause';
-    div_scream.classList.add('hide');
-  };
+    paused = false
+    btn_start.textContent = 'Pause'
+    div_scream.classList.add('hide')
+  }
 
   var pause = function (txt) {
-    paused = true;
-    btn_start.textContent = (txt)? txt:'Unpause';
-  };
+    paused = true
+    btn_start.textContent = (txt)? txt:'Unpause'
+  }
 
   var start = function () {
-    running = true;
-    cycle = 0;
-    unpause();
-    resetSeconds();
-  };
+    running = true
+    cycle = 0
+    unpause()
+    resetSeconds()
+  }
 
   var reset = function () {
-    running = false;
-    btn_start.textContent = 'Start';
-    div_scream.classList.add('hide');
-    resetSeconds();
-  };
+    running = false
+    btn_start.textContent = 'Start'
+    div_scream.classList.add('hide')
+    resetSeconds()
+  }
 
   var hitStart = function () {
     if (running) {
       if (paused) {
-        unpause();
+        unpause()
       } else {
-        pause();
+        pause()
       }
     } else {
-      start();
-    }
-  };
-
-  btn_start.addEventListener('click', hitStart);
-
-  btn_reset.addEventListener('click', function () {
-    reset();
-  });
-
-  in_minutes.addEventListener('change', function () {
-    if (!running) {
-      resetSeconds();
-      updateTimer();
-    }
-  });
-
-  in_break.addEventListener('change', function () {
-    if (!running) {
-      set_break = parseInt(in_break.value);
-    }
-  });
-
-  in_new_member.addEventListener('change', function () {
-    var name = in_new_member.value.trim();
-    if (name) {
-      in_new_member.value = '';
-      members.push(name);
-      updateList();
-    }
-  });
-
-  reset();
-  updateTimer();
-  update();
-
-  if ("Notification" in window) {
-    if (Notification.permission === 'granted') {
-      notify = true;
-    } else if (Notification.permission !== 'denied') {
-      Notification.requestPermission(function (permission) {
-        notify = permission === 'granted';
-      });
+      start()
     }
   }
 
-  var dragged;
+  btn_start.addEventListener('click', hitStart)
+
+  btn_reset.addEventListener('click', function () {
+    reset()
+  })
+
+  in_minutes.addEventListener('change', function () {
+    if (!running) {
+      resetSeconds()
+      updateTimer(seconds)
+    }
+  })
+
+  in_break.addEventListener('change', function () {
+    if (!running) {
+      set_break = parseInt(in_break.value)
+    }
+  })
+
+  in_new_member.addEventListener('change', function () {
+    var name = in_new_member.value.trim()
+    if (name) {
+      in_new_member.value = ''
+      members.push(name)
+      updateList()
+    }
+  })
+
+  reset()
+  updateTimer(seconds)
+  update()
+
+  if ('Notification' in window) {
+    Notification.requestPermission()
+      .then(function(){notify = true})
+      .catch(function(){notify = false})
+  }
+
+  var dragged
 
   document.body.addEventListener('keyup', function (e) {
     if (e.keyCode === 32) {
-      e.preventDefault();
-      hitStart();
+      e.preventDefault()
+      hitStart()
     }
-  });
+  })
 
   document.addEventListener('dragstart', function (e) {
-    dragged = e.target;
+    dragged = e.target
     if (dragged.classList.contains('member_item')) {
-      name = members[memberId(dragged)];
-      e.dataTransfer.setData('text/plain', name);
-      e.dataTransfer.setDragImage(dragged, 0, 0);
-      dragged.style.opacity = 0.5;
+      name = members[memberId(dragged)]
+      e.dataTransfer.setData('text/plain', name)
+      e.dataTransfer.setDragImage(dragged, 0, 0)
+      dragged.style.opacity = 0.5
     }
-  });
+  })
 
   document.addEventListener('dragend', function (e) {
-    dragged = e.target;
+    dragged = e.target
     if (dragged.classList.contains('member_item')) {
-      dragged.style.opacity = '';
+      dragged.style.opacity = ''
     }
-  });
+  })
 
   document.addEventListener('dragover', function (e) {
-    var target = e.target;
+    var target = e.target
     if (target.classList.contains('member_item') && target != dragged) {
-      e.preventDefault();
+      e.preventDefault()
     }
-  });
+  })
 
   document.addEventListener('dragenter', function (e) {
-    var target = e.target;
+    var target = e.target
     if (target.classList.contains('member_item') && target != dragged) {
-      e.preventDefault();
-      target.style.marginBottom = '30px';
+      e.preventDefault()
+      target.style.marginBottom = '30px'
     }
-  });
+  })
 
   document.addEventListener('dragleave', function (e) {
-    var target = e.target;
+    var target = e.target
     if (target.classList.contains('member_item') && target != dragged) {
-      target.style.marginBottom = '';
+      target.style.marginBottom = ''
     }
-  });
+  })
 
   document.addEventListener('drop', function (e) {
-    e.preventDefault();
-    var target = e.target;
+    e.preventDefault()
+    var target = e.target
     if (target.classList.contains('member_item') && target != dragged) {
-      var name = e.dataTransfer.getData('text/plain');
-      var dragged_id = memberId(dragged);
-      var target_id = memberId(target);
+      var name = e.dataTransfer.getData('text/plain')
+      var dragged_id = memberId(dragged)
+      var target_id = memberId(target)
       if (dragged_id > target_id) {
-        members.splice(dragged_id, 1);
-        members.splice(target_id+1, 0, name);
+        members.splice(dragged_id, 1)
+        members.splice(target_id+1, 0, name)
       } else {
-        members.splice(target_id+1, 0, name);
-        members.splice(dragged_id, 1);
+        members.splice(target_id+1, 0, name)
+        members.splice(dragged_id, 1)
       }
-      target.style.marginBottom = '';
-      updateList();
-      updateCycle();
+      target.style.marginBottom = ''
+      updateList()
+      updateCycle()
     }
-  });
-};
+  })
+}
